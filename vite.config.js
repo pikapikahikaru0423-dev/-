9d13f55f81e7afa7d582934103e37978a70b3ab2
田光 <pikapikahikaru0423@gmail.com>
2026-04-27 18:02:10 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/test-1/', 
   plugins: [react()],
-  // GitHub Pages用。自分のPC（ローカル）なら '/'、GitHubなら '/test-1/' に自動切換
-  base: process.env.NODE_ENV === 'production' ? '/test-1/' : '/',
 })
